74c9eb793c9e64588de94ef101a2a62c83fcf1e1
FIVE <cahhong@artfx.fr>
2024-06-09 22:58:00 +0200

    Added Walk

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import os
 directory = ""
 
 albedo = ['albedo','basecolor']
-#for each file in the directory check if it has the word albedo in it
-for filename in os.listdir(directory):
-    for word in albedo:
-        if word in filename:
-            print(f'{filename} :: ALBEDO')
+list_albedo = []
+
+# for each file in the directory and subdirectory check if it has the word albedo in it
+for dirpath, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+        for word in albedo:
+            if word in filename:
+                print(f'{os.path.join(dirpath, filename)} :: ALBEDO')
+                list_albedo.append(os.path.join(dirpath, filename))
+
+print('----')
+print(list_albedo)
